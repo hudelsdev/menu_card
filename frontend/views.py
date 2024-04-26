ad61f9fa8cfb8683b8e5c724b3794bd2cdfcffd4
Hudels Dev <117001322+hudelsdev@users.noreply.github.com>
2024-04-26 15:13:02 +0530

    Update views.py

--- frontend/views.py
+++ frontend/views.py
@@ -3,17 +3,25 @@ from hotel.models import *
 from admin_backend .models import HotelUsers
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
-
+from accounts .model import DeveloperAdmin 
 
 # Create your views here.
 
 def menu_card(request, pk, username):
-    hotel_identifier = request.user.id
-    menu_items = MenuItem.objects.filter(user_id=hotel_identifier)
-    categories = Category.objects.filter(user_id=hotel_identifier)
-    properties = HotelUsers.objects.filter(user_id=hotel_identifier)
-    return render(request, 'frontend/menu_card.html', {'menu_items': menu_items, 'categories': categories,'properties':properties})
+    # Retrieve hotel based on pk and username
+    hotel_identifier = DeveloperAdmin.objects.filter(pk=pk, username=username).first()
 
+    if hotel_identifier:
+        # User is logged in and associated with a hotel
+        menu_items = MenuItem.objects.filter(user_id=hotel_identifier)
+        categories = Category.objects.filter(user_id=hotel_identifier)
+        properties = HotelUsers.objects.filter(user_id=hotel_identifier)
+        return render(request, 'frontend/menu_card.html', {'menu_items': menu_items, 'categories': categories,'properties':properties})
+    else:
+        # User is not logged in or not associated with a hotel (public access)
+        menu_items = MenuItem.objects.all()
+        categories = Category.objects.all()
+        return render(request, 'frontend/public_menu_card.html', {'menu_items': menu_items, 'categories': categories})
 
 
 
