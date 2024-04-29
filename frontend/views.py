7bca1b998684cc790ef81d4222abf4d0ed46a3af
Hudels Dev <117001322+hudelsdev@users.noreply.github.com>
2024-04-29 17:15:25 +0530

    Update views.py

--- frontend/views.py
+++ frontend/views.py
@@ -1,21 +1,16 @@
 from django.shortcuts import render ,redirect
 from hotel.models import *
 from admin_backend .models import HotelUsers
-from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+
 def menu_card(request, pk, username):
-    try:
-        hotel = HotelUsers.objects.get(pk=pk, username=username)
-    except HotelUsers.DoesNotExist:
-        return render(request, '404.html', {'message': 'Hotel not found'})
-
-    menu_items = MenuItem.objects.filter(user=hotel)
-    categories = Category.objects.filter(user=hotel)
-    properties = HotelUsers.objects.filter(pk=hotel.pk)  
-
-    return render(request, 'frontend/menu_card.html', {'menu_items': menu_items, 'categories': categories,'properties': properties})
+    hotel_identifier = request.user.id
+    menu_items = MenuItem.objects.filter(user_id=hotel_identifier)
+    categories = Category.objects.filter(user_id=hotel_identifier)
+    properties = HotelUsers.objects.filter(user_id=hotel_identifier)
+    return render(request, 'frontend/menu_card.html', {'menu_items': menu_items, 'categories': categories,'properties':properties})
 
 
 
